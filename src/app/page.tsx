927a4161edd3551b1bb446acec04e10490deb9e7
Fabroos <signorettafabrizio@gmail.com>
2024-08-08 17:08:40 -0300

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-import {PageView} from "./sarasa";
-
-import {Button} from "@/components/ui/button";
-import {Pump} from ".basehub/react-pump";
-
 export default function HomePage() {
-  return (
-    <div>
-      <Pump
-        queries={[
-          {
-            sarasa: {
-              title: true,
-              _analyticsKey: true,
-            },
-          },
-        ]}
-      >
-        {async ([{sarasa}]) => {
-          return (
-            <div>
-              <PageView _analyticsKey={sarasa._analyticsKey} />
-              <h1>{sarasa.title}</h1>
-              <Button>Click me</Button>
-            </div>
-          );
-        }}
-      </Pump>
-    </div>
-  );
+  return <p>Hello world</p>;
 }
